14b3b10818576d752291365b6bcafe5a2effe861
davyquange <826118993@qq.com>
2024-08-07 11:38:36 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 const headers = Object.fromEntries(Object.entries($request.headers).map(([key, value]) => [key.toLowerCase(), value]));
 Object.assign(headers, {
     'auth-uid': '212989617',
-    'auth-token': '0699840f2378292692aca2cdab71f49a',
+    'auth-token': '6c6f341f8179f4ff9e36102783f5e60a',
 });
 
 $done({ headers });
